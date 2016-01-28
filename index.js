eae564f52dacc3a80e3a5221018f4c1a30546f4a
franz <franzc.coleta@gmail.com>
2016-01-29 00:05:21 +0800

    added sequence generator

---
+++ index.js
@@ -0,0 +1,14 @@
+(function() {
+  "use strict";
+  window.fib = function* genFib(max) {
+    let o = 0,
+      f = 1,
+      s = 1;
+    for (let i = 0; i < max; i++) {
+      o = f + s;
+      f = s;
+      s = o;
+      yield o;
+    }
+  };
+})();
