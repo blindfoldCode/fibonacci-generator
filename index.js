5bb0f6531f025e6ee9b1cd85c314225b81e648ca
franz <franzc.coleta@gmail.com>
2016-01-29 00:40:37 +0800

    added inital sequence will refactor for add each

--- index.js
+++ index.js
@@ -1,22 +1,36 @@
 (function() {
   "use strict";
+
   function* fib(max) {
     let o = 0,
       f = 1,
       s = 1;
-    for (let i = 0; i < max; i++) {
-      o = f + s;
-      f = s;
-      s = o;
-      yield o;
-    }
+      for (let i = 0; i < max; i++) {
+        o = f + s;
+        f = s;
+        s = o;
+        yield o;
+      }
   };
 
 
-    const   input = document.querySelector("input[type=number]").TEXT_NODE,
-    action = document.querySelector("button[name=submit]");
-    console.log(action.textContent);
+  const input = document.querySelector("input[type=number]"),
 
+    action = document.querySelector("button[name=submit]");
+
+  action.addEventListener("mouseup", addSequence)
+  let mode = true;
+
+
+  function addSequence(e) {
+    let done = false,
+      seq = fib(parseInt(input.value));
+    while (!done) {
+      let value = seq.next();
+      done = value.done;
+      if (!done) console.log(value.value, value.done);
+    }
+  }
 
 
 
