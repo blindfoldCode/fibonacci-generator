76065cbb0480026aabe85edf0555d3d56391bbfb
franz <franzc.coleta@gmail.com>
2016-01-29 01:11:15 +0800

    added switchable sequencing

--- index.js
+++ index.js
@@ -5,31 +5,55 @@
     let o = 0,
       f = 1,
       s = 1;
-      for (let i = 0; i < max; i++) {
-        o = f + s;
-        f = s;
-        s = o;
-        yield o;
-      }
+    for (let i = 0; i < max; i++) {
+      o = f + s;
+      f = s;
+      s = o;
+      yield o;
+    }
   };
 
 
   const input = document.querySelector("input[type=number]"),
-
+    result = document.querySelector("#result"),
     action = document.querySelector("button[name=submit]");
 
   action.addEventListener("mouseup", addSequence)
-  let mode = true;
+  let mode = true,
+    done = false,
+    seq, gen;
 
 
   function addSequence(e) {
-    let done = false,
+    if (mode) {
       seq = fib(parseInt(input.value));
-    while (!done) {
-      let value = seq.next();
-      done = value.done;
-      if (!done) console.log(value.value, value.done);
+      action.textContent = "Add";
+      mode = false;
+      gen = seq.next();
+      let appendItem = document.createElement("li");
+      appendItem.innerHTML = gen.value;
+      result.innerHTML = "";
+      result.appendChild(appendItem);
+    } else {
+
+      if (!gen.done) {
+        let appendItem = document.createElement("li");
+        appendItem.innerHTML = gen.value;
+        result.appendChild(appendItem);
+        gen = seq.next();
+        if (gen.done) {
+          action.textContent = "Submit";
+          mode = true;
+        }
+      }
+
     }
+    /*
+        while (!done) {
+          let value = seq.next();
+          done = value.done;
+          if (!done) console.log(value.value, value.done);
+        }*/
   }
 
 
